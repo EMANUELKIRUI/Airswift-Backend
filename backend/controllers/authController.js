077a1789e41e149cd0a0804ce78a0571ce91ebb9
emanuelkiruio87kt <mercyrop003@gmail.com>
2026-04-05 13:40:57 +0000

    Fix authController registration and login flow

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,7 +3,6 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
 const { Op } = require("sequelize");
-const { generateOTP } = require("../utils/generateOTP");
 const { sendEmail } = require("../services/emailService");
 
 const registerUser = async (req, res) => {
