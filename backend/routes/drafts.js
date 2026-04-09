6d59f1a62042ceb6436aa9f365b0fd1f8bdca62b
bismackkip <janemarkee77@gmail.com>
2026-04-09 10:20:13 +0000

    changes

--- backend/routes/drafts.js
+++ backend/routes/drafts.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getDraft, saveDraft, clearDraft } = require('../controllers/draftController');
+const { getDraft, saveDraft, clearDraft, checkDraft } = require('../controllers/draftController');
 const { verifyToken } = require('../middleware/auth');
 
 const router = express.Router();
